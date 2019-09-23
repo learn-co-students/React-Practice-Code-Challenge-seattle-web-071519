fa7970deca3ec7027fa67fc1e05a2c407fa582a1
ayerest <irisiseve@gmail.com>
2019-09-23 13:41:23 -0700

    finished react practice lab

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    let currentFour = this.props.sushi
+    currentFour = this.props.sushi.slice(this.props.index, (this.props.index + 4))
+    console.log("current four sushi", currentFour)
+    return (
+        <div className="belt">
+          {
+            currentFour.map(sushi => {
+              return <Sushi sushi={sushi} key={sushi.id} onEatSushi={this.props.onEatSushi}/>
+            })
+            /* 
+               Render Sushi components here!
+            */
+          }
+          <MoreButton onRotate={this.props.onRotate}/>
+        </div>
+    )
+
+  }
+
 }
 
 export default SushiContainer
